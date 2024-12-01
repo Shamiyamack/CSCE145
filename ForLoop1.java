86c9f4a207a91ac7ad1601bfc8e1c3c8301e1227
Shamiyamack <shamiyammack@gmail.com>
2024-12-01 00:50:35 -0500

    Add files via upload

---
+++ ForLoop1.java
@@ -0,0 +1,20 @@
+
+public class ForLoop1 {
+
+	public static void main(String[] args) {
+		//Printing numbers 0-9
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.println(i);
+		}
+		
+		int i = 0;
+		while(i < 10)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
